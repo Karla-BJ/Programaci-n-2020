bc6b80a1904166618790a44f9fcdb4de3a1c4508
Karla-BJ <karlaberriojalil7@gmail.com>
2020-08-12 13:41:06 -0500

    Código finish

--- Clases/SegundaClase.py
+++ Clases/SegundaClase.py
@@ -1,7 +1,17 @@
 Nombre = input("porfavor ingrese el nombre : ")
-print ("Hola", Nombre, "eres bienvenido a este código")
+print ("Hola", Nombre, "eres bienvenida a este código")
 Edad = int(input("porfavor ingrese su edad : "))
+if(Edad >= 18) : 
+    print ("Ya eres una niña grande")
 Estatura = float(input("porfavor ingrese su estatura : "))
+
+if(Estatura>1.70) :
+        print ("Bendecido y afortunado mor, saludos desde el subsuelo")
+
+if(Estatura<1.70) : 
+    print("Hola, buenas. bienvenido al mundo de las hormigas")
+
+    print("Hasta luego, que tengas un lindo día ♥")
 
 print (Edad)
 print (Estatura)
@@ -10,15 +20,3 @@ print (type(Nombre))
 print (type(Edad))
 print (type(Estatura))
 
-if(Edad >= 18) : 
-    print("Usted es mayor de edad")
-    print ("Ya eres una niña grande")
-
-    if(Estatura>1.70) : 
-    print("Bendecido y afortunado mor, saludos desde el sub suelo")
-
-if(Estatura<1.70) : 
-    print("Hola, buenas. bienvenido al mundo de las hormigas")
-
-
-    print("Hasta luego, que tengas un lindo día ♥")
