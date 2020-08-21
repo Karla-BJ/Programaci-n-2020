ca6e7fff8ddf692b311f72b05ef5fd97c8d8f096
Karla-BJ <karlaberriojalil7@gmail.com>
2020-08-21 13:59:35 -0500

    Parcial 1

---
+++ Exámenes/Parcial1.py
@@ -0,0 +1,24 @@
+print("Hola, bienvenido a la plataforma preventiva de avianca para covid-19. Es un gusto tenerlo como pasajero")
+
+Procedencia=input("Para comenzar, Ingrese su lugar de origen : ")
+
+if(Procedencia== "China" or Procedencia== "Irán" or Procedencia== "Italia"):
+    print("Según las disposiciones a partir de la emergencia sanitaria, usted se encuentra en estado de observación")
+    print("Gracias por la información")
+else:
+    print("Gracias por la información")
+
+Temperatura=float(input("Para continuar con el proceso, porfavor ingrese su temperatura actual : "))
+
+if(Temperatura<36):
+    print("A partir de su temperatura, se encuentra en estado de hipotermia")
+elif(Temperatura>=36 and Temperatura<38.5):
+    print("A partir de su temperatura, se encuentra en estado saludable")
+elif(Temperatura>=38.5 and Temperatura<41):
+    print("A partir de su temperatura, se encuentra en estado de alerta")
+else:
+    print("A partir de su temperatura, se encuentra en estado de peligro")
+
+print("Gracias por utilizar nuestros servicios")
+
+
