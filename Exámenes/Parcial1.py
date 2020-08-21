303b9671a80f5bfe41eba667935212b2347c995f
Karla-BJ <karlaberriojalil7@gmail.com>
2020-08-21 14:17:49 -0500

    Parcial 1 (2,0) definitivo

--- Exámenes/Parcial1.py
+++ Exámenes/Parcial1.py
@@ -5,20 +5,21 @@ Procedencia=input("Para comenzar, Ingrese su lugar de origen : ")
 if(Procedencia== "China" or Procedencia== "Irán" or Procedencia== "Italia"):
     print("Según las disposiciones a partir de la emergencia sanitaria, usted se encuentra en estado de observación")
     print("Gracias por la información")
+
 else:
-    print("Gracias por la información")
+    Temperatura=float(input("Para continuar con el proceso, porfavor ingrese su temperatura actual : "))
+    if(Temperatura<36):
+        print("A partir de su temperatura, se encuentra en estado de hipotermia")
+        print("Gracias por utilizar nuestros servicios")
+    elif(Temperatura>=36 and Temperatura<38.5):
+        print("Gracias por utilizar nuestros servicios")
+        print("A partir de su temperatura, se encuentra en estado saludable")
+    elif(Temperatura>=38.5 and Temperatura<41):
+        print("Gracias por utilizar nuestros servicios")
+        print("A partir de su temperatura, se encuentra en estado de alerta")
+    else:
+        print("A partir de su temperatura, se encuentra en estado de peligro")
+        print("Gracias por utilizar nuestros servicios")
 
-Temperatura=float(input("Para continuar con el proceso, porfavor ingrese su temperatura actual : "))
-
-if(Temperatura<36):
-    print("A partir de su temperatura, se encuentra en estado de hipotermia")
-elif(Temperatura>=36 and Temperatura<38.5):
-    print("A partir de su temperatura, se encuentra en estado saludable")
-elif(Temperatura>=38.5 and Temperatura<41):
-    print("A partir de su temperatura, se encuentra en estado de alerta")
-else:
-    print("A partir de su temperatura, se encuentra en estado de peligro")
-
-print("Gracias por utilizar nuestros servicios")
 
 
