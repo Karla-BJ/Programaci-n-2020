90fcb8c42e3afc7e31e2b01c9769a7fd0d0072dd
Karla-BJ <karlaberriojalil7@gmail.com>
2020-10-21 12:36:40 -0500

    ,

--- Clases/Funciones/agregandoLines.py
+++ Clases/Funciones/agregandoLines.py
@@ -1,4 +1,12 @@
 import funcionesArchivos as helper
-helper.agregarLines ("opinion.txt", "nueva linea")
+nameFile= "libro.txt"
+helper.agregarLinea ("opinion.txt", "nueva linea")
+renglonesLibro = helper.leerArchivos (nameFile )
 
+if (len(renglonesLibro)==0):
+    print ("Es la primera línea que se escribirá en el libro")
+    helper.agregarLinea (nameFile , "El día que programar fue fácil")
+else:
+    linea= input ("ingrese la línea que desee agregar : \n")
+    helper.agregarLinea (nameFile , linea)
 
