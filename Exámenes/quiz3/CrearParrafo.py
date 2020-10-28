f6f86d806be23f3c10108ced13904fdaaf89be84
Karla-BJ <karlaberriojalil7@gmail.com>
2020-10-28 13:58:37 -0500

    Quiz 3 con Laura Montoya ♥

---
+++ Exámenes/quiz3/CrearParrafo.py
@@ -0,0 +1,14 @@
+#Primer punto#
+archivo1= open ("parrafo.txt", "w")
+
+#Segundo punto#
+archivo1= open ("parrafo.txt", "w", encoding  = 'UTF-8')
+archivo1 .write ("Programación es el proceso de tomar un algoritmo \n y codificarlo en una notación, un lenguaje de programación, \n de modo que pueda ser ejecutado por una computadora.\n Aunque existen muchos lenguajes de programación y muchos tipos diferentes de computadoras, \n el primer paso es la necesidad de tener una solución.")
+archivo1.close ()
+
+#Tercer punto#
+listaRenglones=[]
+with open ("parrafo.txt", encoding= "UTF-8") as lines:
+    for line in lines:
+        print (line)
+        listaRenglones.append (line)
